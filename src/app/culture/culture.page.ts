a52116f549ec897f06b6d96d0421fa95e8e16090
mohamed bennouj <medbennouj@gmail.com>
2020-07-17 22:43:22 +0200

    first commit

---
+++ src/app/culture/culture.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-culture',
+  templateUrl: './culture.page.html',
+  styleUrls: ['./culture.page.scss'],
+})
+export class CulturePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/monument/monument.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-monument',
+  templateUrl: './monument.page.html',
+  styleUrls: ['./monument.page.scss'],
+})
+export class MonumentPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
